d0438777521348002f25ab935b518a17d966c707
김관호 김관호 <asdf36597@naver.com>
2024-04-14 22:52:31 +0900

    EnemyFX 만들기전

---
+++ KatanaZero/KatanaZero/PompSlashFX.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "FXBase.h"
+
+// Ό³Έν :
+class APompSlashFX : public AFXBase
+{
+	GENERATED_BODY(AFXBase)
+public:
+	// constructor destructor
+	APompSlashFX();
+	~APompSlashFX();
+
+	// delete Function
+	APompSlashFX(const APompSlashFX& _Other) = delete;
+	APompSlashFX(APompSlashFX&& _Other) noexcept = delete;
+	APompSlashFX& operator=(const APompSlashFX& _Other) = delete;
+	APompSlashFX& operator=(APompSlashFX&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private:
+	float4 Color;
+	FVector Dir = FVector::Zero;
+};
+
